9d0bb4a74d1a895ada2cc51f52ca45fa76e1321d
nakuvalekar <nakuvalekar@gmail.com>
2025-10-24 11:47:37 +0530

    Create program.cs
    Sample test code

---
+++ dotnet-sample/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GitHubActionsLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET GitHub Actions Workflow!");
+        }
+    }
+}
